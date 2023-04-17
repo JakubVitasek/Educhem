21a3936bbcdc0677fb284f10fa2b2d73036448e6
Jakub.Vitasek <Jakub.Vitasek@VT1-17>
2023-04-17 08:28:02 +0200

    Přidejte soubory projektu.

---
+++ Educhem1/VytvorBarvu.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+namespace Educhem1
+{
+    public static partial class Grafika
+    { 
+        public static Color vytvorBarvu()
+        {
+            Random generator = new Random();
+            int cislo1 = generator.Next(255);
+            int cislo2 = generator.Next(255);
+            int cislo3 = generator.Next(255);
+            return Color.FromArgb(cislo1, cislo2, cislo3);
+        }
+    }        
+}
